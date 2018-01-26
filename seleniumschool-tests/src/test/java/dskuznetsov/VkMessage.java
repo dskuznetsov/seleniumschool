15a0524d4f285a63bb81b8f59d77ebefbbf98543
dskuznetsov <30598923+dskuznetsov@users.noreply.github.com>
2018-01-27 00:23:02 +0300

    Fixes (#2)
    * add Utils.class in client, add Credentials.java
    
    * change xpath locators and assertion

--- seleniumschool-tests/src/test/java/dskuznetsov/VkMessage.java
+++ seleniumschool-tests/src/test/java/dskuznetsov/VkMessage.java
@@ -7,10 +7,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-
 import org.aeonbits.owner.ConfigFactory;
 
-import java.util.List;
 
 import static dskuznetsov.Utils.UtilsWebDriver.waitUntilExpectedElementDisplayed;
 import static dskuznetsov.Utils.UtilsWebDriver.waitUntilExpectedTitleDisplayed;
@@ -21,6 +19,8 @@ public class VkMessage {
     private Credentials cfg = ConfigFactory.create(Credentials.class);
     private String vkUserUrl = "https://vk.com/avgribanov";
     private String vkUserName = "Артем Грибанов";
+    private String expectedTitleOfDialogPage = "Диалоги";
+
 
 
     @Before
@@ -37,19 +37,19 @@ public class VkMessage {
     public void sendMessageTest() {
 
         driver.get(vkUserUrl);
-
+        Long currentTime = System.currentTimeMillis() / 1000L;
 
         //Открываем страницу получателя сообщения и логинимся
         waitUntilExpectedTitleDisplayed(driver, vkUserName);
         WebElement loginTextField = driver.findElement(By.xpath("//input[@type='text' and @name='email']"));
         loginTextField.sendKeys(cfg.LOGIN());
-        WebElement passwordTextField = driver.findElement(By.xpath("//input[@type='password' and @id='quick_pass']"));
+        WebElement passwordTextField = driver.findElement(By.xpath("//input[@type='password' and @name='pass']"));
         passwordTextField.sendKeys(cfg.PASSWORD());
         passwordTextField.submit();
 
         //Тап "Написать сообщение"
-        waitUntilExpectedElementDisplayed(driver, "//*[@class='flat_button profile_btn_cut_left']");
-        WebElement quickSendMessageButton = driver.findElement(By.xpath("//*[@class='flat_button profile_btn_cut_left']"));
+        waitUntilExpectedElementDisplayed(driver, "//button[contains(@class,'profile_btn_cut_left')]");
+        WebElement quickSendMessageButton = driver.findElement(By.xpath("//button[contains(@class,'profile_btn_cut_left')]"));
         quickSendMessageButton.click();
 
         //Тап "Перейти к диалогу"
@@ -57,24 +57,13 @@ public class VkMessage {
         WebElement openDialogPageButton = driver.findElement(By.xpath("//*[@class='mail_box_header_link']"));
         openDialogPageButton.click();
 
-        //Подсчет количества сообщений на странице до отправки сообщения
-        waitUntilExpectedTitleDisplayed(driver, "Диалоги");
-        List<WebElement> msgOnPageBeforeSend = driver.findElements(By.xpath("//div[contains(@class,'im-mess--text wall_module _im_log_body')]"));
-        System.out.println(msgOnPageBeforeSend.size());
-
         //Находим поля для ввода текста, вводим сообщение и отправляем по кнопке "Отправить"
-        WebElement messageTextField = driver.findElement(By.xpath("//*[@id='im_editable16435589']"));
-        messageTextField.sendKeys("Привет");
-        WebElement sendMessageButton = driver.findElement(By.xpath("//button[@class='im-send-btn im-chat-input--send _im_send im-send-btn_send']"));
+        waitUntilExpectedTitleDisplayed(driver, expectedTitleOfDialogPage);
+        WebElement messageTextField = driver.findElement(By.xpath("//div[contains(@class,'im-chat-input--text')][@role='textbox']"));
+        messageTextField.sendKeys(String.valueOf(currentTime));
+        WebElement sendMessageButton = driver.findElement(By.xpath("//button[contains(@class,'im-send-btn_send')]"));
         sendMessageButton.click();
 
-        //Подсчет количества сообщений на странице после отправки сообщения
-        List<WebElement> msgOnPageAfterSend = driver.findElements(By.xpath("//div[contains(@class,'im-mess--text wall_module _im_log_body')]"));
-        System.out.println(msgOnPageAfterSend.size());
-
-        //Количество сообщений на странице увеличилось на 1
-        assertThat(msgOnPageAfterSend.size() - msgOnPageBeforeSend.size()).isEqualTo(1);
-
+        assertThat(driver.findElement(By.xpath("//li[last()]//div[@class='im-mess--text wall_module _im_log_body' and text()='" + currentTime + "']")).isDisplayed()).isTrue();
     }
 }
-
